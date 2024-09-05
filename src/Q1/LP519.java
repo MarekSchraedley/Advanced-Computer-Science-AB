f72b7d1432cf4ca3f3a1d7d5bc087810f8fcd0d6
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-09-05 12:18:00 -0500

    update 9/3/2024

--- src/Q1/LP519.java
+++ src/Q1/LP519.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 public class LP519 {
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
+        var input = new Scanner(System.in);
         System.out.print("Enter a scentence: ");
-        String scentence = scanner.next();
+        String scentence = input.nextLine();
         System.out.print("Enter a word: " );
-        String word = scanner.next();
+        String word = input.nextLine();
         scentence = scentence.substring(0, scentence.indexOf(word)) + scentence.substring(scentence.indexOf(word)+word.length());
         System.out.println(scentence);
     }

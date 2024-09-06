e8fcac7d2c5013bd36bbad363e8ddaa3890e27af
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-09-06 12:05:59 -0500

    update 9/3/2024

---
+++ src/Q1/LP521.java
@@ -0,0 +1,12 @@
+package Q1;
+import java.util.Scanner;
+public class LP521 {
+    public static void main(String[] args) {
+        var input = new Scanner(System.in);
+        System.out.print("Enter your first name: ");
+        String firstname = input.nextLine();
+        System.out.println("Enter your last name: ");
+        String lastname = input.nextLine();
+
+    }
+}

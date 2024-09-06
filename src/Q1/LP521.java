73a28f015f3300e5005396f1be58e30474ec607f
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-09-06 12:18:01 -0500

    update 9/3/2024

--- src/Q1/LP521.java
+++ src/Q1/LP521.java
@@ -5,8 +5,16 @@ public class LP521 {
         var input = new Scanner(System.in);
         System.out.print("Enter your first name: ");
         String firstname = input.nextLine();
-        System.out.println("Enter your last name: ");
+        System.out.print("Enter your last name: ");
         String lastname = input.nextLine();
-
+        if (lastname.substring(0, 1).compareTo("I") <= 0) {
+            System.out.println(firstname + " " + lastname + " Is assigned to Group 1");
+        }
+        else if (lastname.substring(0, 1).compareTo("S") <= 0) {
+            System.out.println(firstname + " " + lastname + " Is assigned to Group 2");
+        }
+        else {
+            System.out.println(firstname + " " + lastname + " Is assigned to Group 3");
+        }
     }
 }

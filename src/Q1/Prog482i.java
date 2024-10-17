7e17b838fd7150683aa573271c1cc9ec98be4a01
MarekSchraedley <schraedl.m@stu.janesville.k12.wi.us>
2024-10-17 16:46:53 -0500

    update

---
+++ src/Q1/Prog482i.java
@@ -0,0 +1,36 @@
+package Q1;
+
+import Algorithims.SortingAlgorithms;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog482i {
+    public static void main(String[] args) {
+        try {
+            var file = new Scanner(new File("Langdat/numsort.dat"));
+            int count = 0;
+            while (file.hasNext()) {
+                file.nextInt();
+                count++;
+            }
+            file = new Scanner(new File("Langdat/numsort.dat"));
+            Integer[] array = new Integer[count];
+            for (int lcv = 0; lcv < array.length; lcv++) {
+                array[lcv] = file.nextInt();
+            }
+            for (int lcv = 0; lcv < array.length; lcv++) {
+                System.out.print(array[lcv] + " ");
+            }
+            System.out.println("\n");
+            SortingAlgorithms.selectionSort(array);
+            for (int lcv = 0; lcv < array.length; lcv++) {
+                System.out.print(array[lcv] + " ");
+            }
+            file.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}

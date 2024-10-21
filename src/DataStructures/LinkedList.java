7b8661ee3d20b3c19ee45fdc1136924b4789889b
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-10-21 12:18:03 -0500

    update 9/3/2024

--- src/DataStructures/LinkedList.java
+++ src/DataStructures/LinkedList.java
@@ -147,117 +147,117 @@ public class LinkedList<T extends Comparable<T>> {
         return false;
     }
 
-    public IntLinkedList getReverse() {
-        var current = head;
-        LinkedList myList = new LinkedList();
-        while (current.next != null) {
-            myList.addFront(current.data);
-            current = current.next;
-        }
-        return myList;
-    }
-
-    public double avg() {
-        var current = head;
-        int average = 0;
-        while (current.next != null) {
-            average += current.data;
-            current = current.next;
-        }
-        return  average/getCount();
-    }
-
-    public int min() {
-        var current = head;
-        int min = current.data;
-        while (current.next != null) {
-            if (current.data < min) {
-                min = current.data;
-            }
-            current = current.next;
-        }
-        return min;
-    }
-
-    public int max() {
-        var current = head;
-        int max = current.data;
-        while (current.next != null) {
-            if (current.data > max) {
-                max = current.data;
-            }
-            current = current.next;
-        }
-        return max;
-    }
-
-    public int indexOfMax() {
-        var current = head;
-        int max = max();
-        int count = 0;
-        while (current.next != null) {
-            if (current.data == max) {
-                return count;
-            }
-            current = current.next;
-            count++;
-        }
-        return -1;
-    }
-
-    public int lastIndexOfMax() {
-        var current = head;
-        int max = max();
-        int count = 0;
-        int index = -1;
-        while (current.next != null) {
-            if (current.data == max) {
-                index = count;
-            }
-            current = current.next;
-            count++;
-        }
-        return index;
-    }
-
-    public void sort() {
-
-    }
-
-    public int remove58() {
-        var current = head;
-        int count = 0;
-        while (current.next.next != null) {
-            if (current.next.data == 58) {
-                current.next = current.next.next;
-                count++;
-            } else {
-                current = current.next;
-            }
-        }
-        return count;
-    }
-
-    public int getEvenCount() {
-        var current = head;
-        int count = 0;
-        while (current.next != null) {
-            if (current.data % 2 == 0) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-
-    public void removeOdds() {
-        var current = head;
-        while (current.next.next != null) {
-            if (current.next.data % 2 == 1) {
-                current.next = current.next.next;
-            } else {
-                current = current.next;
-            }
-        }
-    }
+//    public IntLinkedList getReverse() {
+//        var current = head;
+//        LinkedList myList = new LinkedList();
+//        while (current.next != null) {
+//            myList.addFront(current.data);
+//            current = current.next;
+//        }
+//        return myList;
+//    }
+//
+//    public double avg() {
+//        var current = head;
+//        int average = 0;
+//        while (current.next != null) {
+//            average += current.data;
+//            current = current.next;
+//        }
+//        return  average/getCount();
+//    }
+//
+//    public int min() {
+//        var current = head;
+//        int min = current.data;
+//        while (current.next != null) {
+//            if (current.data < min) {
+//                min = current.data;
+//            }
+//            current = current.next;
+//        }
+//        return min;
+//    }
+//
+//    public int max() {
+//        var current = head;
+//        int max = current.data;
+//        while (current.next != null) {
+//            if (current.data > max) {
+//                max = current.data;
+//            }
+//            current = current.next;
+//        }
+//        return max;
+//    }
+//
+//    public int indexOfMax() {
+//        var current = head;
+//        int max = max();
+//        int count = 0;
+//        while (current.next != null) {
+//            if (current.data == max) {
+//                return count;
+//            }
+//            current = current.next;
+//            count++;
+//        }
+//        return -1;
+//    }
+//
+//    public int lastIndexOfMax() {
+//        var current = head;
+//        int max = max();
+//        int count = 0;
+//        int index = -1;
+//        while (current.next != null) {
+//            if (current.data == max) {
+//                index = count;
+//            }
+//            current = current.next;
+//            count++;
+//        }
+//        return index;
+//    }
+//
+//    public void sort() {
+//
+//    }
+//
+//    public int remove58() {
+//        var current = head;
+//        int count = 0;
+//        while (current.next.next != null) {
+//            if (current.next.data == 58) {
+//                current.next = current.next.next;
+//                count++;
+//            } else {
+//                current = current.next;
+//            }
+//        }
+//        return count;
+//    }
+//
+//    public int getEvenCount() {
+//        var current = head;
+//        int count = 0;
+//        while (current.next != null) {
+//            if (current.data % 2 == 0) {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+//
+//
+//    public void removeOdds() {
+//        var current = head;
+//        while (current.next.next != null) {
+//            if (current.next.data % 2 == 1) {
+//                current.next = current.next.next;
+//            } else {
+//                current = current.next;
+//            }
+//        }
+//    }
 }

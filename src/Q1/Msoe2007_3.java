27ed23ab1c4f2665cad6fd94e20d934cad77d1c8
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-10-23 12:17:25 -0500

    update 9/3/2024

---
+++ src/Q1/Msoe2007_3.java
@@ -0,0 +1,21 @@
+package Q1;
+
+import java.util.Scanner;
+
+public class Msoe2007_3 {
+    public static void main(String[] args) {
+        var input = new Scanner(System.in);
+        System.out.println("Enter angle A: ");
+        double A = input.nextDouble();
+        System.out.println("Enter angle B: ");
+        double B = input.nextDouble();
+        System.out.println("Enter angle C: ");
+        double C = input.nextDouble();
+        System.out.println("Enter the length of the side opposite angle C: ");
+        if (A+B+C < 180-1 || A+B+C > 180+1) {
+            System.out.println("ERROR: angles do not add up to 180");//Not finbished
+        } else {
+            System.out.println("The radius of a circle inscribed in the triangle is " + (c * Math.sin(A/2) * Math.sin(B/2) * Math.));
+        }
+    }
+}

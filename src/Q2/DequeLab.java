97fefe307adebe44d7a7a1e427c6ad9460b81a27
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-11-05 12:17:19 -0600

    update 9/3/2024

---
+++ src/Q2/DequeLab.java
@@ -0,0 +1,34 @@
+package Q2;
+
+import DataStructures.Deque;
+
+public class DequeLab {
+    public static void main(String[] args) {
+        var deque = new Deque<Integer>();
+        for (int i = 0; i < 10; i++) {
+            deque.enqueueFront(i);
+        }
+        for (int i = 10; i < 20; i++) {
+            deque.enqueueRear(i);
+        }
+
+        System.out.println(deque.size());
+        System.out.println(deque.peekFront());
+        System.out.println(deque.peekRear());
+        System.out.println(deque.dequeueFront());
+        System.out.println(deque.dequeueRear());
+        System.out.println(deque.size());
+
+        var rand = new java.util.Random(42);
+        for (int i = 0; i < 50; i++) {
+            deque.enqueueFront(rand.nextInt(50));
+        }
+        for (int i = 0; i < 48; i++) {
+            deque.dequeueFront();
+        }
+        System.out.println("New size: " + deque.size());
+        System.out.println(deque.peekFront());
+        System.out.println(deque.peekRear());
+
+    }
+}

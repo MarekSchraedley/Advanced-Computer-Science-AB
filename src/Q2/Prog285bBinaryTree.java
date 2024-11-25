2d3e5a3aca545cf85b5dd1f12ebf088d6795a249
schraedl.m <schraedl.m@stu.janesville.k12.wi.us>
2024-11-25 12:17:26 -0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/Q2/Prog285bBinaryTree.java
@@ -0,0 +1,35 @@
+package Q2;
+
+import DataStructures.BinarySearchTree;
+import DataStructures.LinkedListStack;
+import Q1.Prog285b.Cl285b;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog285bBinaryTree {
+    public static void main(String[] args) {
+        try {
+            var file = new Scanner(new File("Langdat/prog285b.dat"));
+            var tree = new BinarySearchTree<Cl285b>();
+
+            System.out.println("Id\tCode\tSales\tCommission");
+            while (file.hasNext()) {
+                int id = file.nextInt();
+                int c = file.nextInt();
+                double s = file.nextDouble();
+
+                var fred = new Cl285b(id, c, s);
+                tree.insert(fred);
+            }
+            file.close();
+
+            tree.printInOrder();
+        } catch (
+                IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+}

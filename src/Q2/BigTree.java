55feda0565eba0f4c579078f5cb9a202ff8b61a4
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2024-12-09 12:16:21 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree$Node.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/CircularDeqeue.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Msoe2024_8.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Pelican526CircleDeque.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog285bBinaryTree.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog602bQueue.class

--- src/Q2/BigTree.java
+++ src/Q2/BigTree.java
@@ -1,12 +1,16 @@
 package Q2;
 
 import DataStructures.BinarySearchTree;
-
+import java.math.*;
 public class BigTree {
     public static void main(String[] args) {
         var tree = new BinarySearchTree<Integer>();
-        //for (int i = 0; i < ; i++) {
-            
-       // }
+        int temp = 0;
+        for (int i = 0; i < 50; i++) {
+            temp = (int)(Math.random()*100+1);
+            System.out.print(temp + " ");
+            tree.insert(temp);
+        }
+        tree.printInOrder();
     }
 }

40c57f4f7046eaceec7c6bbcdd11cba24927bb2c
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2024-12-19 12:15:48 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree$Node.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/CircularDeqeue.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Msoe2024_8.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Pelican526CircleDeque.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog285bBinaryTree.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog602bQueue.class

--- src/Q2/MSOE2024_7.java
+++ src/Q2/MSOE2024_7.java
@@ -1,6 +1,13 @@
 package Q2;
 
+import java.util.ArrayList;
+
 public class MSOE2024_7 {
+    public static int findN(ArrayList<String> words) {
+        return 0;
+    }
 
+    public static void main(String[] args) {
 
+    }
 }

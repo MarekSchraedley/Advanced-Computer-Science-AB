4fad0550c2fb5bf366852dd5fef1530e91583b35
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2025-01-02 12:16:42 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree$Node.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/CircularDeqeue.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Msoe2024_8.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Pelican526CircleDeque.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog285bBinaryTree.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog602bQueue.class

--- src/Q2/MSOE2024_7.java
+++ src/Q2/MSOE2024_7.java
@@ -3,10 +3,19 @@ package Q2;
 import java.util.ArrayList;
 
 public class MSOE2024_7 {
-    public static int findN(ArrayList<String> words) {
-        return 0;
+    public class gift {
+        String myName;
+        Double myLength;
+        Double myWidth;
+        Double myHeight;
+        public gift(String name, double length, double width, double height) {
+            myName = name;
+            myLength = length;
+            myWidth = width;
+            myHeight = height;
+        }
+        public String getMyName{return myName;}
     }
-
     public static void main(String[] args) {
 
     }

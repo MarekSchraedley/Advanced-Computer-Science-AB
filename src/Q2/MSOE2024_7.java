21e2bcdd5ab23a0436ed5f1b858219fbbe9632ae
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2025-01-03 12:14:25 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree$Node.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/CircularDeqeue.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Msoe2024_8.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Pelican526CircleDeque.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog285bBinaryTree.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog602bQueue.class

--- src/Q2/MSOE2024_7.java
+++ src/Q2/MSOE2024_7.java
@@ -1,9 +1,12 @@
 package Q2;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MSOE2024_7 {
-    public class gift {
+    public static class gift {
         String myName;
         Double myLength;
         Double myWidth;
@@ -14,9 +17,34 @@ public class MSOE2024_7 {
             myWidth = width;
             myHeight = height;
         }
-        public String getMyName{return myName;}
+        public String getMyName(){return myName;}
+        public Double getMyLength(){return myLength;}
+        public Double getMyWidth(){return myWidth;}
+        public Double getMyHeight(){return myHeight;}
     }
     public static void main(String[] args) {
-
+        try {
+            var file = new Scanner(new File("Langdat/REPLACE.dat"));
+            ArrayList<gift> giftList = new ArrayList<>();
+            while (file.hasNext()) {
+                String tempName = file.next();
+                String temp = file.next();
+                int lcv = 0;
+                while (temp.substring(lcv, lcv+1) != "x") {
+                    lcv++;
+                }
+                int length = Integer.parseInt(temp.substring(0, lcv));
+                int lcv2 = lcv;
+                while (temp.substring(lcv2, lcv2+1) != "x") {
+                    lcv2++;
+                }
+                int width = Integer.parseInt(temp.substring(lcv+1, lcv2));
+                int height = Integer.parseInt(temp.substring(lcv2+1));
+                giftList.add(new gift(tempName, length, width, height));
+            }
+            file.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }

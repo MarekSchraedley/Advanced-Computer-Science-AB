7278666e041a50eeb42f5ba352823b378da6fead
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2025-02-10 12:16:13 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree$Node.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/CircularDeqeue.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Msoe2024_8.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Pelican526CircleDeque.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog285bBinaryTree.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog602bQueue.class

--- src/Q3/Prog505a.java
+++ src/Q3/Prog505a.java
@@ -1,5 +1,43 @@
 package Q3;
 
-public class Prog505a {
+import DataStructures.Set;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog505a {
+    public static void main(String[] args) {
+        try {
+            var file = new Scanner(new File("Langdat/prog505a.txt"));
+            var set = new Set<Integer>();
+            while (file.hasNext()) {
+                set.insert(file.nextInt());
+            }
+            System.out.println("Reading Contest\nName\t\tBooks\tPoints");
+            var iter = set.iterator();
+            while (iter.hasNext()) {
+                int total = 0;
+                var quantity = iter.next();
+                System.out.println("Quantity: " + quantity);
+                for (int i = 0; i < quantity; i++) {
+                    if (i  > 0 && i <= 3) {
+                        total += 10;
+                    } else if (i >= 4 && i <= 6) {
+                        total += 15;
+                    } else if (i >= 7) {
+                        total += 20;
+                    }
+                }
+                //System.out.println("Price Per: " + price);
+                //System.out.println("Price: " + (price * quantity));
+                System.out.println();
+            }
+
+
+            file.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }

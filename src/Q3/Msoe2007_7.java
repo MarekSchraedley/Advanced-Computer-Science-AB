688d7226834e15471e3e6ec9d454ad31462f27c2
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2025-03-13 12:17:55 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree$Node.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/BinarySearchTree.class
    #	out/production/Advanced-Computer-Science-AB/DataStructures/CircularDeqeue.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Msoe2024_8.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Pelican526CircleDeque.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog285bBinaryTree.class
    #	out/production/Advanced-Computer-Science-AB/Q2/Prog602bQueue.class

---
+++ src/Q3/Msoe2007_7.java
@@ -0,0 +1,33 @@
+package Q3;
+
+import java.util.Scanner;
+
+public class Msoe2007_7 {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("What is the number of digits in the odometer? ");
+        int digits = scanner.nextInt();
+        System.out.println("What is the current reading? ");
+        String reading = scanner.next();
+        boolean isPalendrome = false;
+        String closestPal = "";
+        for (int i = 0; i < digits-reading.length(); i++) {closestPal += "0";}
+        closestPal += reading;
+        int count = -1;
+        String revPal = "";
+        while (!isPalendrome) {
+            count++;
+            revPal = "";
+            for (int i = closestPal.length()-1; i >= 0; i++) {
+                revPal += closestPal.substring(i, i+1);
+            }
+            if (closestPal.equals(revPal)) {
+                isPalendrome = false;
+            }
+            String closestPalTemp = closestPal;
+            closestPal = "";
+            for (int i = 0; i < digits-reading.length(); i++) {closestPal += "0";}
+            closestPal += closestPalTemp;
+        }
+    }
+}

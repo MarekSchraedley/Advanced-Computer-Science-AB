53c8cf2068569985df9c5ea0b6c326b2e3553645
MarekSchraedley <schraedl.m@stu.janesville.k12.wi.us>
2025-03-29 17:03:56 -0500

    Merge remote-tracking branch 'origin/master'

--- src/Q3/Msoe2009_8.java
+++ src/Q3/Msoe2009_8.java
@@ -5,6 +5,17 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Msoe2009_8 {
+    public static String[][] adjacentFill(int x, int y, String[][] array, String replaceWith) {
+        array[x][y] = replaceWith;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (array[x+i][y+j].equals("*")) {
+                    array = adjacentFill(x+i, y+j, array, replaceWith);
+                }
+            }
+        }
+        return array;
+    }
 
     public static void main(String[] args) {
         try {
@@ -12,15 +23,50 @@ public class Msoe2009_8 {
             int count = file.nextInt();
             int length = file.nextInt();
             String line = "";
-            for (int i = 0; i < count-1; i++) {
+            String[][] array = new String[count][length];
+            file.nextLine();
+            for (int i = 0; i < count; i++) {
                 line = file.nextLine();
-                for (int j = 0; j < line.length(); j++) {
-
+                for (int j = 0; j < length; j++) {
+                    array[i][j] = line.substring(j, j+1);
                 }
             }
+            String[] chars = {"A", "B", "C", "D"};
+            int charsCount = 0;
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[0].length; j++) {
+                    if (array[i][j].equals("*")) {
+                        array = adjacentFill(i, j, array, chars[charsCount]);
+                        charsCount++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[0].length; j++) {
+                    System.out.print(array[i][j]);
+                }
+                System.out.println();
+            }
+
             file.close();
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
 }
+/*
+
+  AA   AA  BBBB
+   AA AA  B
+   A A A   BBB
+   A   A      B
+  AA   AA BBBB
+
+    CCC   DDDDD
+   C   C  D
+   C   C  DDD
+   C   C  D
+    CCC   DDDDD
+
+ */
